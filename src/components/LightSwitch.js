21608c1a8c7244674e940d61398e58c2a8a28332
badogan <basri.dogan.71@gmail.com>
2020-01-28 11:01:40 +0000

    Done.

---
+++ src/components/LightSwitch.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class LightSwitch extends React.Component {
+
+    constructor(){
+        super();
+        this.state ={
+            toggled: false
+        }
+    }
+
+    handleClick = () => {
+        this.setState(previousState => {
+            return{
+                toggled: !previousState.toggled
+            }
+        })
+    }
+
+    render(){
+        return(
+        <button onClick={this.handleClick}>{this.state.toggled ? "ON": "OFF"}</button>
+        )
+    }
+
+}
+
+export default LightSwitch;
